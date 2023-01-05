624cab3fa8c5dba82fe8d6d713cf125608586829
BernardKwesi <bernarddormon31@gmail.com>
2023-01-05 20:43:20 +0000

    Add dummy data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import Header from './Header';
+import Footer from './Footer';
+import Employees from './Employees';
 
 export default function App() {
   return (
     <main>
-      React ⚛️ + Vite ⚡ + Replit 🌀
+      <Header />
+      <Employees />
+      <Footer />
     </main>
   )
 }
---
+++ src/Content.jsx
@@ -0,0 +1,11 @@
+const Content = () => {
+
+  return (
+    <div>
+      <p>Main Content</p>
+    </div>
+
+  );
+}
+
+export default Content;
