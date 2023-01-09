8c337b3e375035b1c4a8aa44e98e28bb0d085bce
BernardKwesi <bernarddormon31@gmail.com>
2023-01-09 19:36:19 +0000

    pass props to header

--- src/App.jsx
+++ src/App.jsx
@@ -112,7 +112,13 @@ export default function App() {
 
   return (
     <main>
-      <Header />
+      <Header
+        selectedTeam={selectedTeam}
+        teamMemberCount={
+          employees.filter((employee) => employee.teamName === selectedTeam)
+            .length
+        }
+      />
       <Employees
         employees={employees}
         selectedTeam={selectedTeam}
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,14 @@
-const Header = () => {
-
+const Header = ({ selectedTeam, teamMemberCount }) => {
   return (
-    <header>
-      <h1 className="">Header</h1>
+    <header className="container">
+      <div className="row justify-content-center mt-3 mb-3">
+        <h1 className="">Team Member Allocation</h1>
+        <h3>
+          {selectedTeam} has {teamMemberCount}
+        </h3>
+      </div>
     </header>
-
   );
-}
+};
 
 export default Header;
